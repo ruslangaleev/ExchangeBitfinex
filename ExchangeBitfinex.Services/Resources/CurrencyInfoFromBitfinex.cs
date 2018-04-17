07e31645ea320ae8fe67a7e41b690a2746408aee
ruslan.nafisovich@gmail.com <ruslan.nafisovich@gmail.com>
2018-04-17 07:58:19 +0300

    Добавлен клиент для Bitfinex'a

---
+++ ExchangeBitfinex.Services/Resources/CurrencyInfoFromBitfinex.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ExchangeBitfinex.Services.Resources
+{
+    public class CurrencyInfoFromBitfinex
+    {
+        public decimal LastPrice { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+}
---
+++ ExchangeBitfinex.Services/Resources/ResponseBitfinex.cs
@@ -0,0 +1,9 @@
+﻿namespace ExchangeBitfinex.Services.Resources
+{
+    public class ResponseBitfinex
+    {
+        public string last_price { get; set; }
+
+        public string timestamp { get; set; }
+    }
+}
